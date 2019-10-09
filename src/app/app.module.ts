b9eeb693120a1077b0a43717ba5f752461ca1fcf
Yashwardhan Pauranik <pauranik51@gmail.com>
2019-10-09 13:27:48 +0530

    chore: Ngx-webstorage removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {CookieService} from 'ngx-cookie-service';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {GraphQLModule} from './graphql/graphql.module';
+import {NavbarModule} from './modules/navbar/navbar.module';
 import {SharedComponentsModule} from './shared/components/shared-components.module';
 import {SharedDirectivesModule} from './shared/directives/shared-directives.module';
-import { CookieService } from 'ngx-cookie-service';
-import { NavbarModule } from './modules/navbar/navbar.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { NavbarModule } from './modules/navbar/navbar.module';
     NavbarModule
   ],
   providers: [
-    CookieService,
+    CookieService
   ],
   bootstrap: [AppComponent]
 })
