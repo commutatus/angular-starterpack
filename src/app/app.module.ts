1846a60d8161f3caa3cbe68d8ba5d74ea45fb258
Yashwardhan Pauranik <pauranik51@gmail.com>
2019-10-23 22:45:50 +0530

    refactor: Un-used component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+
 import {CookieService} from 'ngx-cookie-service';
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {GraphQLModule} from './graphql/graphql.module';
